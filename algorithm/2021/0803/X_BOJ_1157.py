e96c10816d6cb2508033b9ffd313589eb18fea2c
minchae9 <itzmeee@naver.com>
2021-08-03 20:34:58 +0900

    2021 폴더로 정리

---
+++ algorithm/2021/0803/X_BOJ_1157.py
@@ -0,0 +1,15 @@
+# chars = input().upper()
+# mx = chars.count(chars[0])
+# ans = []
+# for char in chars:
+#     if char in ans:
+#         pass
+#     else:
+#         if chars.count(char) > mx:
+#             ans=[char]
+#         elif chars.count(char) == mx:
+#             ans.append(char)
+# if len(ans) > 1:
+#     print('?')
+# else:
+#     print(ans[0])
