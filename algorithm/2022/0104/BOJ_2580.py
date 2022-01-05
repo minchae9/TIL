da155ce95fb4da2ab7a37307ba59ddbdf6d902cc
minchae9 <itzmeee@naver.com>
2022-01-05 18:14:38 +0900

    BOJ 2580: 재시도

--- algorithm/2022/0104/BOJ_2580.py
+++ algorithm/2022/0104/BOJ_2580.py
@@ -31,32 +31,25 @@ while zero_count > 0:
                     zero_count -= 1
                     break
     # 3x3 정사각형
-    x = y = 0
-
-    z_r, z_c = 0, 0
-    while x < 3:
-        visited = [0] * 9
-        for i in range(3):
-            row = 3*x + i
-            for j in range(3):
-                col = 3*y + j
-                if sudoku[row][col] > 0:
-                    visited[sudoku[row][col]-1] += 1
+    """
+    8 1 2
+    7 ^ 3
+    6 5 4
+    """
+    dx, dy = [0, -1, -1, 0, 1, 1, 1, 0, -1], [0, 0, 1, 1, 1, 0, -1, -1, -1] # 위에서부터 시계방향
+    for i in range(1, 9, 3):
+        for j in range(1, 9, 3):
+            check = [0] * 9
+            z_x = z_y = 0
+            for d in range(9):
+                nx, ny = i+dx[d], j+dy[d]
+                if sudoku[nx][ny] > 0:
+                    check[sudoku[nx][ny]-1] += 1
                 else:
-                    z_r = row
-                    z_c = col
-        if sum(visited) == 8:
-            sudoku[z_r][z_c] = visited.index(0)+1
-            zero_count -=1
-        y += 1
-        if y == 3:
-            y = 0
-            x += 1
-    """
-    행 012 열 012, 345, 678
-    행 345 열 012, 345, 678
-    행 678 열 012, 345, 678
-    """
+                    z_x, z_y = nx, ny
+            if sum(check) == 8:
+                sudoku[z_x][z_y] = check.index(0) + 1
+                zero_count -= 1
 
 # 출력
 for a in range(9):
---
+++ algorithm/2022/0105/BOJ_2580.py
@@ -0,0 +1,61 @@
+"""
+pypy3로 돌림.
+
+참고 사이트: https://hongcoding.tistory.com/118
+- 해야할 일이 많을 땐 함수 단위로 나누기
+- 여기서도 dfs 사용할 수 있음을 배우기
+"""
+import sys
+input = sys.stdin.readline
+
+# 스도쿠 맵 받기
+sudoku = [[] for _ in range(9)]
+for n in range(9):
+    sudoku[n] = list(map(int, input().split()))
+# 0 위치 모은 거: blank
+blank = []
+for r in range(9):
+    for c in range(9):
+        if sudoku[r][c] == 0:
+            blank.append((r, c))
+
+def row(x, val):
+    for i in range(9):
+        if val == sudoku[x][i]:
+            return False
+    return True
+
+def column(y, val):
+    for j in range(9):
+        if val == sudoku[j][y]:
+            return False
+    return True
+
+def sqr(x, y, val):
+    # 좌측 상단 칸
+    row = x // 3 * 3
+    col = y // 3 * 3
+    for r in range(3):
+        for c in range(3):
+            if val == sudoku[row+r][col+c]:
+                return False
+    return True
+
+def dfs(i):
+    if i == len(blank):
+        # 출력
+        for p in range(9):
+            # for q in range(9):
+            #     print(sudoku[p][q], end=' ')
+            # print()
+            print(*sudoku[p])
+        return
+    else:
+        for num in range(1, 10):
+            x, y = blank[i][0], blank[i][1]
+            if row(x, num) and column(y, num) and sqr(x, y, num):
+                sudoku[x][y] = num
+                dfs(i+1)
+                sudoku[x][y] = 0
+
+dfs(0)
